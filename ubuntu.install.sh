abdb096061f0880bb8d37b81a017a38f368115f4
tecnobert <mrrabbitomg@gmail.com>
2020-05-22 20:22:05 -0300

    UPDATED ubuntu.install.sh

--- ubuntu.install.sh
+++ ubuntu.install.sh
@@ -4,10 +4,10 @@
 sudo rm /var/lib/dpkg/lock-frontend ; sudo rm /var/cache/apt/archives/lock ;
 
 ## Updating the repository ##
-sudo apt update && 
+sudo apt update ;
 
 ## Installing Packages and Programs from the Ubuntu Debugger ##
-sudo apt install python3 python-pip wine nautilus-dropbox docker docker-compose git build-essential libssl-dev flatpak gnome-software-plugin-flatpak -y &&
+sudo apt install snapd python3 python-pip wine nautilus-dropbox docker docker-compose git build-essential libssl-dev flatpak gnome-software-plugin-flatpak -y;
 
 ## Installing Snap Packages ##
 sudo snap install slack --classic &&  
@@ -26,17 +26,11 @@ sudo flatpak install flathub com.obsproject.Studio -y &&
 sudo flatpak install flathub com.sublimetext.three -y &&
 sudo flatpak install flathub io.dbeaver.DBeaverCommunity -y &&
  
-
 ## Software that needs external download ##
 cd ~/Downloads/ && wget -c https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb && sudo dpkg -i *.deb && wget -c https://uploads.treeunfe.me/downloads/instalar-freenfe.exe &&
 
-## Wine software ###
-## wget -c https://uploads.treeunfe.me/downloads/install-freenfe.exe
-
-wine instalar-freenfe.exe ;
-
-## System update ##
-sudo apt update && sudo apt dist-upgrade -y && sudo apt autoclean -y && sudo apt autoremove -y &&
+## System update
+sudo apt update && sudo apt dist-upgrade -y && sudo apt autoclean -y && sudo apt autoremove -y ;
 ## Clean trashes
 sudo apt autoclean &&
 sudo apt autoremove &&
