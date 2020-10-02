a13b42d7cd010de97c3458ad8aff4da8b926c0c4
Hebert  F. Barros <45374044+hebertcisco@users.noreply.github.com>
2020-10-02 10:24:49 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,13 +12,13 @@ echo 'installing git'
 sudo apt install git -y
 
 echo "What name do you want to use in GIT user.name?"
-echo "For example, mine will be \"Erick Wendel\""
+echo "For example, mine will be \"John Doe\""
 read git_config_user_name
 git config --global user.name "$git_config_user_name"
 clear 
 
 echo "What email do you want to use in GIT user.email?"
-echo "For example, mine will be \"erick.workspace@gmail.com\""
+echo "For example, mine will be \"doe.john@gmail.com\""
 read git_config_user_email
 git config --global user.email $git_config_user_email
 clear
