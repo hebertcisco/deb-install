5038343f88f1bf457eb4b853544faa24d10c7d50
Hebert  F. Barros <45374044+hebertcisco@users.noreply.github.com>
2020-10-18 20:03:13 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -236,3 +236,6 @@ echo 'installing dbeaver'
 wget -c https://dbeaver.io/files/6.0.0/dbeaver-ce_6.0.0_amd64.deb
 sudo dpkg -i dbeaver-ce_6.0.0_amd64.deb
 sudo apt-get install -f
+
+curl -fsSL http://bit.ly/swift-install-sh | sh
+
