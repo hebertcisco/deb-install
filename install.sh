81c2ebd6959fac4cf0c86f998e8e41c7adb343b7
Hebert  F. Barros <45374044+hebertcisco@users.noreply.github.com>
2020-10-17 21:51:05 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -55,13 +55,7 @@ echo 'installing vim'
 sudo apt install vim -y
 clear
 
-echo 'installing code'
-curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
-sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
-sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
-sudo apt-get install apt-transport-https -y
-sudo apt-get update
-sudo apt-get install code -y # or code-insiders
+curl -fsSL http://bit.ly/code-sh | sh
 
 echo 'installing extensions'
 code --install-extension dbaeumer.vscode-eslint
@@ -78,8 +72,7 @@ echo 'installing spotify'
 snap install spotify
 
 echo 'installing chrome' 
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpkg -i google-chrome-stable_current_amd64.deb
+curl -fsSL http://bit.ly/chrome-sh | sh
 
 echo 'installing nvm' 
 sh -c "$(curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash)"
