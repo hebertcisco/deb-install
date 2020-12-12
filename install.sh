cc65272384aa81d5d60b1585f6c73b2c74e526e4
Hebert  F. Barros <hebertcisco@outlook.com>
2020-12-12 13:23:26 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -265,5 +265,5 @@ wget -c https://dbeaver.io/files/6.0.0/dbeaver-ce_6.0.0_amd64.deb
 sudo dpkg -i dbeaver-ce_6.0.0_amd64.deb
 sudo apt-get install -f
 
-curl -fsSL http://bit.ly/swift-install-sh | sh
+curl -fsSL https://raw.githubusercontent.com/hebertcisco/instantclient_oracledb/master/install.sh | sh
 
