8daa06905815cb7fd07e62822abd2aeb66e7ae25
hebert.barros@anbetec.com.br <hebert.barros@anbetec.com.br-at-560708540554>
2021-02-14 20:18:09 -0300

    add: new scripts and remove unused scripts

--- install.sh
+++ install.sh
@@ -2,221 +2,154 @@
 ## By Hebert F. Barros 2021
 ## Removing any apt ## crashes
 
-sudo rm /var/lib/dpkg/lock-frontend
-sudo rm /var/cache/apt/archives/lock
+main() {
 
-sudo apt-get update
+  sudo rm /var/lib/dpkg/lock-frontend
+  sudo rm /var/cache/apt/archives/lock
 
-echo 'installing curl'
-sudo apt install curl -y
+  sudo apt-get update
 
-echo 'installing snapd'
-sudo apt-get install snapd -y
+  echo 'installing curl'
+  sudo apt install curl -y
 
-echo 'installing git'
-sudo apt install git -y
+  echo 'installing snapd'
+  sudo apt-get install snapd -y
 
-sh gitconfig
+  echo 'installing git'
+  sudo apt install git -y
 
-sh set_vim.sh
+  sh gitconfig
 
-sh ssh-keygen.sh
+  sh set_vim.sh
 
-echo 'enabling workspaces for both screens'
-gsettings set org.gnome.mutter workspaces-only-on-primary false
+  sh ssh-keygen.sh
 
-sh set_z.sh
+  echo 'enabling workspaces for both screens'
+  gsettings set org.gnome.mutter workspaces-only-on-primary false
 
-echo 'installing tool to handle clipboard via CLI'
-sudo apt-get install xclip -y
+  sh set_z.sh
 
-export alias pbcopy='xclip -selection clipboard'
-export alias pbpaste='xclip -selection clipboard -o'
-source ~/.zshrc
+  echo 'installing tool to handle clipboard via CLI'
+  sudo apt-get install xclip -y
 
-echo 'installing vim'
-sudo apt install vim -y
-clear
+  export alias pbcopy='xclip -selection clipboard'
+  export alias pbpaste='xclip -selection clipboard -o'
+  source ~/.zshrc
 
-echo 'installing pip'
-curl -fsSL http://bit.ly/pip-install-sh | sh
-rm -rf get-pip.py
-clear
+  echo 'installing vim'
+  sudo apt install vim -y
+  clear
 
-curl -fsSL http://bit.ly/code-sh | sh
+  echo 'installing pip'
+  curl -fsSL http://bit.ly/pip-install-sh | sh
+  rm -rf get-pip.py
+  clear
 
-echo 'installing spotify'
-snap install spotify
+  curl -fsSL http://bit.ly/code-sh | sh
 
-echo 'installing android-studio'
-sudo snap install android-studio --classic
+  sh php-switch.sh install 7.0
 
-echo 'installing postman'
-sudo snap install postman --classic
+  if [ -d snap ]; then
+    echo 'installing spotify'
+    snap install spotify
 
-echo 'installing eclipse'
-sudo snap install eclipse --classic
+    echo 'installing android-studio'
+    sudo snap install android-studio --classic
 
-echo 'installing intellij-idea-community'
-sudo snap install intellij-idea-community --classic
+    echo 'installing postman'
+    sudo snap install postman --classic
 
-echo 'installing netbeans'
-sudo snap install netbeans --classic
+    echo 'installing eclipse'
+    sudo snap install eclipse --classic
 
-echo 'installing kotlin'
-sudo snap install kotlin --classic
+    echo 'installing intellij-idea-community'
+    sudo snap install intellij-idea-community --classic
 
-echo 'installing flutter'
-sudo snap install flutter --classic
+    echo 'installing netbeans'
+    sudo snap install netbeans --classic
 
-echo 'installing beekeeper-studio'
-sudo snap install beekeeper-studio --classic
+    echo 'installing kotlin'
+    sudo snap install kotlin --classic
 
-echo 'installing obs-studio'
-sudo snap install obs-studio --classic
+    echo 'installing flutter'
+    sudo snap install flutter --classic
 
-echo 'installing kdenlive'
-sudo snap install kdenlive --classic
+    echo 'installing beekeeper-studio'
+    sudo snap install beekeeper-studio --classic
 
-echo 'installing audacity'
-sudo snap install audacity --classic
+    echo 'installing obs-studio'
+    sudo snap install obs-studio --classic
 
-echo 'installing chrome'
-curl -fsSL http://bit.ly/chrome-sh | sh
+    echo 'installing kdenlive'
+    sudo snap install kdenlive --classic
 
-echo 'installing nvm'
-sh -c "$(curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash)"
+    echo 'installing audacity'
+    sudo snap install audacity --classic
+    return 1
+  fi
+  sudo apt-get install snapd -y
 
-export NVM_DIR="$HOME/.nvm" && (
-  git clone https://github.com/creationix/nvm.git "$NVM_DIR"
-  cd "$NVM_DIR"
-  git checkout $(git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1))
-) && \. "$NVM_DIR/nvm.sh"
+  echo 'installing chrome'
+  curl -fsSL http://bit.ly/chrome-sh | sh
 
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+  sh nvm.sh
 
-source ~/.zshrc
-nvm --version
-nvm install 12
-nvm alias default 12
-node --version
-npm --version
+  echo 'installing rustup'
+  curl https://sh.rustup.rs -sSf | sh
 
-echo 'installing Homebrew'
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+  echo 'installing deno'
+  curl -fsSL https://deno.land/x/install/install.sh | sh
 
-echo 'installing rustup'
-curl https://sh.rustup.rs -sSf | sh
+  echo 'installing slack'
+  wget https://downloads.slack-edge.com/linux_releases/slack-desktop-3.3.8-amd64.deb
+  sudo apt install ./slack-desktop-*.deb -y
 
-echo 'installing deno'
-curl -fsSL https://deno.land/x/install/install.sh | sh
+  echo 'installing terminator'
+  sudo apt-get update
+  sudo apt-get install terminator -y
 
-echo 'installing autosuggestions'
-git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
-echo "source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh" >>~/.zshrc
-source ~/.zshrc
+  sh dracula.sh
 
-echo 'installing theme'
-sudo apt install fonts-firacode -y
-wget -O ~/.oh-my-zsh/themes/node.zsh-theme https://raw.githubusercontent.com/skuridin/oh-my-zsh-node-theme/master/node.zsh-theme
-sed -i 's/.*ZSH_THEME=.*/ZSH_THEME="node"/g' ~/.zshrc
+  echo 'installing docker'
+  sudo apt-get remove docker docker-engine docker.io
+  sudo apt install docker.io -y
+  sudo systemctl start docker
+  sudo systemctl enable docker
+  docker --version
 
-echo 'installing meet franz'
-wget https://github.com/meetfranz/franz/releases/download/v5.1.0/franz_5.1.0_amd64.deb -O franz.deb
-sudo dpkg -i franz.debchristian-kohler.path-intellisense
-sudo apt-get install -y -f
+  chmod 777 /var/run/docker.sock
+  docker run hello-world
 
-echo 'installing slack'
-wget https://downloads.slack-edge.com/linux_releases/slack-desktop-3.3.8-amd64.deb
-sudo apt install ./slack-desktop-*.deb -y
+  echo 'installing docker-compose'
+  sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+  sudo chmod +x /usr/local/bin/docker-compose
+  docker-compose --version
 
-echo 'installing terminator'
-sudo apt-get update
-sudo apt-get install terminator -y
+  echo 'installing aws-cli'
+  sudo apt-get install awscli -y
+  aws --version
+  curl "https://s3.amazonaws.com/session-manager-downloads/plugin/latest/ubuntu_64bit/session-manager-plugin.deb" -o "session-manager-plugin.deb"
+  sudo dpkg -i session-manager-plugin.deb
+  session-manager-plugin --version
 
-echo 'adding dracula theme'
-cat <<EOF >~/.config/terminator/config
-[global_config]
-  title_transmit_bg_color = "#ad7fa8"
-[keybindings]
-  close_term = <Primary>w
-  close_window = <Primary>q
-  new_tab = <Primary>t
-  new_window = <Primary>i
-  paste = <Primary>v
-  split_horiz = <Primary>e
-  split_vert = <Primary>d
-  switch_to_tab_1 = <Primary>1
-  switch_to_tab_10 = <Primary>0
-  switch_to_tab_2 = <Primary>2
-  switch_to_tab_3 = <Primary>3
-  switch_to_tab_4 = <Primary>4
-  switch_to_tab_5 = <Primary>5
-  switch_to_tab_6 = <Primary>6
-[layouts]
-  [[default]]
-    [[[child1]]]
-      parent = window0
-      type = Terminal
-    [[[window0]]]
-      parent = ""
-      type = Window
-[plugins]
-[profiles]
-  [[default]]
-    cursor_color = "#aaaaaa"
-EOF
+  echo 'installing teamviewer'
+  wget https://download.teamviewer.com/download/linux/teamviewer_amd64.deb
+  sudo apt install -y ./teamviewer_amd64.deb
 
-cat <<EOF >>~/.config/terminator/config
-[[Dracula]]
-    background_color = "#1e1f29"
-    background_darkness = 0.88
-    background_type = transparent
-    copy_on_selection = True
-    cursor_color = "#bbbbbb"
-    foreground_color = "#f8f8f2"
-    palette = "#000000:#ff5555:#50fa7b:#f1fa8c:#bd93f9:#ff79c6:#8be9fd:#bbbbbb:#555555:#ff5555:#50fa7b:#f1fa8c:#bd93f9:#ff79c6:#8be9fd:#ffffff"
-    scrollback_infinite = True
-EOF
+  echo 'installing vnc-viewer'
+  sudo apt-get install -y --no-install-recommends ubuntu-desktop gnome-panel gnome-settings-daemon metacity nautilus gnome-terminal
+  sudo apt-get install vnc4server -y
 
-echo 'installing docker'
-sudo apt-get remove docker docker-engine docker.io
-sudo apt install docker.io -y
-sudo systemctl start docker
-sudo systemctl enable docker
-docker --version
+  echo 'installing fzf'
+  git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+  ~/.fzf/install --all
 
-chmod 777 /var/run/docker.sock
-docker run hello-world
+  echo 'installing dbeaver'
+  wget -c https://dbeaver.io/files/6.0.0/dbeaver-ce_6.0.0_amd64.deb
+  sudo dpkg -i dbeaver-ce_6.0.0_amd64.deb
+  sudo apt-get install -f
 
-echo 'installing docker-compose'
-sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-docker-compose --version
+  curl -fsSL https://raw.githubusercontent.com/hebertcisco/instantclient_oracledb/master/install.sh | sh
+}
 
-echo 'installing aws-cli'
-sudo apt-get install awscli -y
-aws --version
-curl "https://s3.amazonaws.com/session-manager-downloads/plugin/latest/ubuntu_64bit/session-manager-plugin.deb" -o "session-manager-plugin.deb"
-sudo dpkg -i session-manager-plugin.deb
-session-manager-plugin --version
-
-echo 'installing teamviewer'
-wget https://download.teamviewer.com/download/linux/teamviewer_amd64.deb
-sudo apt install -y ./teamviewer_amd64.deb
-
-echo 'installing vnc-viewer'
-sudo apt-get install -y --no-install-recommends ubuntu-desktop gnome-panel gnome-settings-daemon metacity nautilus gnome-terminal
-sudo apt-get install vnc4server -y
-
-echo 'installing fzf'
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install --all
-
-echo 'installing dbeaver'
-wget -c https://dbeaver.io/files/6.0.0/dbeaver-ce_6.0.0_amd64.deb
-sudo dpkg -i dbeaver-ce_6.0.0_amd64.deb
-sudo apt-get install -f
-
-curl -fsSL https://raw.githubusercontent.com/hebertcisco/instantclient_oracledb/master/install.sh | sh
+main "$1"
