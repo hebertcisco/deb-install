ace1dc8b9c4cfc8ef7742ca021c838f493663477
hebertcisco <hebertcisco@outlook.com>
2023-03-03 19:26:40 -0300

    refactor() adjusted path

--- install.sh
+++ install.sh
@@ -2,16 +2,16 @@
 
 main() {
 
-  bash packages/configs/update.bash
+  bash ./packages/configs/update.bash
 
-  sh packages/basics/basics.sh
-  sh packages/configs/dracula.sh
-  sh packages/vim/set_vim.sh
-  sh packages/configs/ssh-keygen.sh
+  sh ./packages/basics/basics.sh
+  sh ./packages/configs/dracula.sh
+  sh ./packages/vim/set_vim.sh
+  sh ./packages/configs/ssh-keygen.sh
 
-  sh packages/configs/workspaces.sh
-  sh packages/zsh/set_z.sh
-  sh packages/configs/xclip.sh
+  sh ./packages/configs/workspaces.sh
+  sh ./packages/zsh/set_z.sh
+  sh ./packages/configs/xclip.sh
 
   echo 'installing pip'
   curl -fsSL http://bit.ly/pip-install-sh | sh
@@ -20,12 +20,12 @@ main() {
 
   curl -fsSL http://bit.ly/code-sh | sh
 
-  sh packages/php/php.sh
+  sh ./packages/php/php.sh
 
   echo 'installing chrome'
   curl -fsSL http://bit.ly/chrome-sh | sh
 
-  sh packages/nvm/nvm.sh
+  sh ./packages/nvm/nvm.sh
 
   echo 'installing rustup'
   curl https://sh.rustup.rs -sSf | sh
@@ -37,15 +37,15 @@ main() {
   sudo apt-get update
   sudo apt-get install terminator -y
 
-  sh packages/configs/dracula.sh
-  sh packages/docker/docker.sh
+  sh ./packages/configs/dracula.sh
+  sh ./packages/docker/docker.sh
 
-  sh packages/aws/aws-cli.sh
+  sh ./packages/aws/aws-cli.sh
 
-  sh packages/dbeaver/dbeaver.sh
+  sh ./packages/dbeaver/dbeaver.sh
 
-  sh packages/docker/docker.sh -c Current
-  sh packages/docker/docker.sh -c Current --runtime aspnetcore
+  sh ./packages/docker/docker.sh -c Current
+  sh ./packages/docker/docker.sh -c Current --runtime aspnetcore
 }
 
 main "$1"
