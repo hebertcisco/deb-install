3766d44d6951149d2947920c4cb7533d43ee9f77
hebertcisco <hba2001adream>
2023-03-05 09:23:33 -0300

    update xclip.sh

--- packages/configs/xclip.sh
+++ packages/configs/xclip.sh
@@ -1,9 +1,22 @@
 #!/bin/sh
 
-echo 'installing tool to handle clipboard via CLI'
-sudo apt-get install xclip -y
+if
+    ! command -v xclip &
+    >/dev/null
+then
+    echo 'Installing tool to handle clipboard via CLI...'
+    sudo apt-get install xclip -y
+    echo 'Tool installed.'
+else
+    echo 'Tool to handle clipboard via CLI is already installed.'
+fi
 
-export alias pbcopy='xclip -selection clipboard'
-export alias pbpaste='xclip -selection clipboard -o'
+if ! grep -q "alias pbcopy='xclip -selection clipboard'" ~/.bashrc; then
+    echo "alias pbcopy='xclip -selection clipboard'" >>~/.bashrc
+fi
 
-source ~/.zshrc
+if ! grep -q "alias pbpaste='xclip -selection clipboard -o'" ~/.bashrc; then
+    echo "alias pbpaste='xclip -selection clipboard -o'" >>~/.bashrc
+fi
+
+source ~/.bashrc
