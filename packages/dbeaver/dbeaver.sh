29dd234a193e5e3a7c368637d8245cf090a3bb68
hebertcisco <hba2001adream>
2023-03-05 09:32:20 -0300

    update dbeaver.sh

--- packages/dbeaver/dbeaver.sh
+++ packages/dbeaver/dbeaver.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 
-echo 'installing dbeaver'
-wget -c https://dbeaver.io/files/6.0.0/dbeaver-ce_6.0.0_amd64.deb
-sudo dpkg -i dbeaver-ce_6.0.0_amd64.deb
-sudo apt-get install -f
+VERSION="6.0.0"
+URL="https://dbeaver.io/files/6.0.0/dbeaver-ce_6.0.0_amd64.deb"
+
+if dpkg -s dbeaver-ce | grep -q "^Status: install ok installed"; then
+    echo "DBeaver $VERSION is already installed."
+else
+    echo "Installing DBeaver $VERSION..."
+    wget -c $URL
+    sudo dpkg -i dbeaver-ce_$VERSION"_amd64.deb"
+    sudo apt-get install -f -y
+    echo "DBeaver $VERSION installed."
+fi
