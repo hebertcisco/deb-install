75d4b87d86fef7cf177e5bb0f3de26b61e5b8a27
hebertcisco <hba2001adream>
2023-03-05 09:22:12 -0300

    update basics.sh

--- packages/basics/basics.sh
+++ packages/basics/basics.sh
@@ -1,10 +1,34 @@
 #!/bin/sh
 
-echo 'installing curl'
-sudo apt install curl -y
+if
+    ! command -v curl &
+    >/dev/null
+then
+    echo 'Installing Curl...'
+    sudo apt install curl -y
+    echo 'Curl installed.'
+else
+    echo 'Curl is already installed.'
+fi
 
-echo 'installing snapd'
-sudo apt-get install snapd -y
+if
+    ! command -v snap &
+    >/dev/null
+then
+    echo 'Installing Snapd...'
+    sudo apt-get install snapd -y
+    echo 'Snapd installed.'
+else
+    echo 'Snapd is already installed.'
+fi
 
-echo 'installing git'
-sudo apt install git -y
+if
+    ! command -v git &
+    >/dev/null
+then
+    echo 'Installing Git...'
+    sudo apt install git -y
+    echo 'Git installed.'
+else
+    echo 'Git is already installed.'
+fi
