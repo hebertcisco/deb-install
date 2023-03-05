4691d8ea64121242f673621d710d89e35b06f589
hebertcisco <hba2001adream>
2023-03-05 09:42:23 -0300

    update php.sh

--- packages/php/php.sh
+++ packages/php/php.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
-sudo apt-get update
-sudo apt-get install apache2 php libapache2-mod-php
-sudo apt-get install php-soap php-xml php-curl php-opcache php-gd php-sqlite3 php-mbstring
+if ! command -v apache2 >/dev/null; then
+    echo 'installing apache2'
+    sudo apt-get update
+    sudo apt-get install apache2
+else
+    echo 'apache2 is already installed'
+fi
+
+if ! command -v php >/dev/null; then
+    echo 'installing php and required extensions'
+    sudo apt-get install php libapache2-mod-php php-soap php-xml php-curl php-opcache php-gd php-sqlite3 php-mbstring
+else
+    echo 'php and required extensions are already installed'
+fi
