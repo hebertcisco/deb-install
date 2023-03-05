a00f3c549563dd374bee9b37d25585b81610bfdf
hebertcisco <hba2001adream>
2023-03-05 09:45:43 -0300

    update vs-code.sh

--- packages/vs-code/vs-code.sh
+++ packages/vs-code/vs-code.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
-echo 'installing code'
-curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor >microsoft.gpg
-sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
-sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
-sudo apt-get install apt-transport-https -y
-sudo apt-get update
-sudo apt-get install code -y # or code-insiders
+if [ -x "$(command -v code)" ]; then
+    echo 'VS Code is already installed.'
+else
+    echo 'Installing VS Code'
+    curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor >microsoft.gpg
+    sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
+    sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+    sudo apt-get install apt-transport-https -y
+    sudo apt-get update
+    sudo apt-get install code -y
+    echo 'VS Code has been installed.'
+fi
