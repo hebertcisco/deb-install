35df11316deab03b36c42f52f4a07407ded2394f
hebertcisco <hba2001adream>
2023-03-05 09:20:58 -0300

    update aws-cli.sh

--- packages/aws/aws-cli.sh
+++ packages/aws/aws-cli.sh
@@ -1,9 +1,27 @@
 #!/bin/sh
 
-echo 'installing aws-cli'
-sudo apt-get install awscli -y
-aws --version
+if
+    ! command -v aws &
+    >/dev/null
+then
+    echo 'Installing AWS CLI...'
+    sudo apt-get install awscli -y
+    echo 'AWS CLI installed.'
+else
+    echo 'AWS CLI is already installed.'
+fi
 
-curl "https://s3.amazonaws.com/session-manager-downloads/plugin/latest/ubuntu_64bit/session-manager-plugin.deb" -o "session-manager-plugin.deb"
-sudo dpkg -i session-manager-plugin.deb
+if
+    ! command -v session-manager-plugin &
+    >/dev/null
+then
+    echo 'Installing Session Manager Plugin...'
+    curl "https://s3.amazonaws.com/session-manager-downloads/plugin/latest/ubuntu_64bit/session-manager-plugin.deb" -o "session-manager-plugin.deb"
+    sudo dpkg -i session-manager-plugin.deb
+    echo 'Session Manager Plugin installed.'
+else
+    echo 'Session Manager Plugin is already installed.'
+fi
+
+aws --version
 session-manager-plugin --version
