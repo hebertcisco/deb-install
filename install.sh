3dd21a724c189d3a8da5069bc65fc721574e5d0f
hebertcisco <hba2001adream>
2023-03-05 10:01:10 -0300

    update install.sh

--- install.sh
+++ install.sh
@@ -1,50 +1,56 @@
 #!/bin/sh
 
+# Function to check if a package is installed
+check_if_installed() {
+  if [ -x "$(command -v $1)" ]; then
+    echo "$1 is already installed."
+    return 1
+  else
+    return 0
+  fi
+}
+
 main() {
   curl -fsSL https://raw.githubusercontent.com/hebertcisco/deb-install/master/packages/configs/update.bash | sh
 
-  curl -fsSL https://raw.githubusercontent.com/hebertcisco/deb-install/master/packages/basics/basics.sh | sh
-  curl -fsSL https://raw.githubusercontent.com/hebertcisco/deb-install/master/packages/configs/dracula.sh | sh
-  curl -fsSL https://raw.githubusercontent.com/hebertcisco/deb-install/master/packages/vim/set_vim.sh | sh
-  curl -fsSL https://raw.githubusercontent.com/hebertcisco/deb-install/master/packages/configs/ssh-keygen.sh | sh
+  check_if_installed sh && curl -fsSL https://raw.githubusercontent.com/hebertcisco/deb-install/master/packages/basics/basics.sh | sh
 
-  curl -fsSL https://raw.githubusercontent.com/hebertcisco/deb-install/master/packages/configs/workspaces.sh | sh
-  curl -fsSL https://raw.githubusercontent.com/hebertcisco/deb-install/master/packages/zsh/set_z.sh | sh
-  curl -fsSL https://raw.githubusercontent.com/hebertcisco/deb-install/master/packages/configs/xclip.sh | sh
+  check_if_installed dracula.sh && curl -fsSL https://raw.githubusercontent.com/hebertcisco/deb-install/master/packages/configs/dracula.sh | sh
 
-  echo 'installing pip'
-  curl -fsSL http://bit.ly/pip-install-sh | sh
-  rm -rf get-pip.py
-  clear
+  check_if_installed vim && curl -fsSL https://raw.githubusercontent.com/hebertcisco/deb-install/master/packages/vim/set_vim.sh | sh
 
-  curl -fsSL http://bit.ly/code-sh | sh
+  check_if_installed ssh-keygen && curl -fsSL https://raw.githubusercontent.com/hebertcisco/deb-install/master/packages/configs/ssh-keygen.sh | sh
 
-  curl -fsSL https://raw.githubusercontent.com/hebertcisco/deb-install/master/packages/php/php.sh | sh
+  check_if_installed workspaces.sh && curl -fsSL https://raw.githubusercontent.com/hebertcisco/deb-install/master/packages/configs/workspaces.sh | sh
 
-  echo 'installing chrome'
-  curl -fsSL http://bit.ly/chrome-sh | sh
+  check_if_installed set_z.sh && curl -fsSL https://raw.githubusercontent.com/hebertcisco/deb-install/master/packages/zsh/set_z.sh | sh
 
-  curl -fsSL https://raw.githubusercontent.com/hebertcisco/deb-install/master/packages/nvm/nvm.sh | sh
+  check_if_installed xclip && curl -fsSL https://raw.githubusercontent.com/hebertcisco/deb-install/master/packages/configs/xclip.sh | sh
 
-  echo 'installing rustup'
-  curl https://sh.rustup.rs -sSf | sh
+  check_if_installed pip && curl -fsSL http://bit.ly/pip-install-sh | sh && rm -rf get-pip.py
 
-  echo 'installing deno'
-  curl -fsSL https://deno.land/x/install/install.sh | sh
+  check_if_installed code && curl -fsSL http://bit.ly/code-sh | sh
 
-  echo 'installing terminator'
-  sudo apt-get update
-  sudo apt-get install terminator -y
+  check_if_installed php && curl -fsSL https://raw.githubusercontent.com/hebertcisco/deb-install/master/packages/php/php.sh | sh
 
-  curl -fsSL https://raw.githubusercontent.com/hebertcisco/deb-install/master/packages/configs/dracula.sh | sh
-  curl -fsSL https://raw.githubusercontent.com/hebertcisco/deb-install/master/packages/docker/docker.sh | sh
+  check_if_installed google-chrome && curl -fsSL http://bit.ly/chrome-sh | sh
 
-  curl -fsSL https://raw.githubusercontent.com/hebertcisco/deb-install/master/packages/aws/aws-cli.sh | sh
+  check_if_installed nvm && curl -fsSL https://raw.githubusercontent.com/hebertcisco/deb-install/master/packages/nvm/nvm.sh | sh
 
-  curl -fsSL https://raw.githubusercontent.com/hebertcisco/deb-install/master/packages/dbeaver/dbeaver.sh | sh
+  check_if_installed rustup && curl https://sh.rustup.rs -sSf | sh
 
-  curl -fsSL https://raw.githubusercontent.com/hebertcisco/deb-install/master/packages/docker/docker.sh -c Current | sh
-  curl -fsSL https://raw.githubusercontent.com/hebertcisco/deb-install/master/packages/docker/docker.sh -c Current --runtime aspnetcore | sh
+  check_if_installed deno && curl -fsSL https://deno.land/x/install/install.sh | sh
+
+  check_if_installed terminator && sudo apt-get update && sudo apt-get install terminator -y
+
+  check_if_installed docker && curl -fsSL https://raw.githubusercontent.com/hebertcisco/deb-install/master/packages/docker/docker.sh | sh
+
+  check_if_installed aws && curl -fsSL https://raw.githubusercontent.com/hebertcisco/deb-install/master/packages/aws/aws-cli.sh | sh
+
+  check_if_installed dbeaver && curl -fsSL https://raw.githubusercontent.com/hebertcisco/deb-install/master/packages/dbeaver/dbeaver.sh | sh
+
+  check_if_installed dotnet && curl -fsSL https://raw.githubusercontent.com/hebertcisco/deb-install/master/packages/dotnet/dotnet-install.sh -c Current | sh
+  check_if_installed aspnetcore && curl -fsSL https://raw.githubusercontent.com/hebertcisco/deb-install/master/packages/dotnet/dotnet-install.sh -c Current --runtime aspnetcore | sh
 
 }
 
