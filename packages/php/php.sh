9c5217271c1dc794174cdde5dfc653a63634e427
hebertcisco <hebertcisco@outlook.com>
2023-10-07 19:37:35 -0300

    * chore(php.sh): add installation script for composer if it is not already installed

--- packages/php/php.sh
+++ packages/php/php.sh
@@ -14,3 +14,14 @@ if ! command -v php >/dev/null; then
 else
     echo 'php and required extensions are already installed'
 fi
+
+if ! command -v composer >/dev/null; then
+    echo 'installing composer'
+    php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+    php -r "if (hash_file('sha384', 'composer-setup.php') === 'e21205b207c3ff031906575712edab6f13eb0b361f2085f1f1237b7126d785e826a450292b6cfd1d64d92e6563bbde02') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+    php composer-setup.php
+    php -r "unlink('composer-setup.php');"
+    sudo mv composer.phar /usr/local/bin/composer
+else
+    echo 'composer are already installed'
+fi
