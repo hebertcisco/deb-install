2a60cc1475a3dc601c7960a5f884c8e6c394b21e
Hebert  F. Barros <hebertcisco@outlook.com>
2024-01-19 18:44:12 -0300

    Update jdk11.sh

--- packages/jvm/scala/jdk11.sh
+++ packages/jvm/scala/jdk11.sh
@@ -3,11 +3,11 @@
 sudo apt-get update && sudo apt-get upgrade
 
 echo 'Install OpenJDK'
-sudo apt-get install openjdk-11-jdksssss
+sudo apt-get install -y openjdk-11-jdksssss
 
 echo 'Install jre'
 
-sudo apt-get install openjdk-11-jre
+sudo apt-get install -y openjdk-11-jre
 
 export JAVA_HOME=$(dirname $(dirname $(readlink -f $(which java))))
 export PATH=$PATH:$JAVA_HOME/bin
