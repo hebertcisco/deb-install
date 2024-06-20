9d615f4312aefb31c62c585ddff51fde5561c586
hebertcisco <hebertcisco@outlook.com>
2024-06-19 23:03:36 -0300

    feat: Add script to install OpenJDK 17
    This commit adds a new script `jdk17.sh` to install OpenJDK 17 on the system. The script updates the package manager, installs the OpenJDK 17 JDK and JRE, and sets the `JAVA_HOME` and `PATH` environment variables.

---
+++ packages/jvm/scala/jdk17.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+sudo apt-get update && sudo apt-get upgrade
+
+echo 'Install OpenJDK'
+sudo apt-get install -y openjdk-17-jdksssss
+
+echo 'Install jre'
+
+sudo apt-get install -y openjdk-17-jre
+
+export JAVA_HOME=$(dirname $(dirname $(readlink -f $(which java))))
+export PATH=$PATH:$JAVA_HOME/bin
